787c791a1ed2a82bf9ec70075cc05b6c22f8646d
Trevor Beasty <trevor.beasty@gmail.com>
2017-06-22 15:04:09 -0400

    create classes responsible for representing vendor types in a list

---
+++ Uncharted/Uncharted/TJBVendorOptionsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TJBVendorOptionsListTableViewCell.swift
+//  Uncharted
+//
+//  Created by Trevor Beasty on 6/21/17.
+//  Copyright © 2017 BeastlyCreations. All rights reserved.
+//
+
+import UIKit
+
+class TJBVendorOptionsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
