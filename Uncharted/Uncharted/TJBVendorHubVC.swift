56bdbda125028a0c570bc4a33f78d1acd26aeb1f
Trevor Beasty <trevor.beasty@gmail.com>
2017-06-23 12:35:06 -0400

    add dismissal custom transition to vendor hub scene

--- Uncharted/Uncharted/TJBVendorHubVC.swift
+++ Uncharted/Uncharted/TJBVendorHubVC.swift
@@ -13,23 +13,52 @@ class TJBVendorHubVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        addSceneTransitionGestureRecognizer()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func addSceneTransitionGestureRecognizer() {
+        
+        let gr = UIScreenEdgePanGestureRecognizer(target: self,
+                                                  action: #selector(didPanRightFromLeftEdge(gr:)))
+        gr.edges = UIRectEdge.left
+        view.addGestureRecognizer(gr)
     }
-    */
+}
 
+// custom transition
+extension TJBVendorHubVC {
+    
+    func didPanRightFromLeftEdge(gr: UIScreenEdgePanGestureRecognizer) {
+        guard let mapBasedSearchVC = presentingViewController as? TJBMapBasedSearchVC else { return }
+        let interactor = mapBasedSearchVC.interactor
+        
+        let translation = gr.translation(in: view)
+        let horizontalMovement = translation.x / view.bounds.width
+        let rightMovement = fmaxf(Float(horizontalMovement), 0.0)
+        let rightMovementPercent = fminf(rightMovement, 1.0)
+        let progress = CGFloat(rightMovementPercent)
+        
+        switch gr.state {
+            
+        case .began:
+            interactor.hasStarted = true
+            mapBasedSearchVC.dismiss(animated: true, completion: nil)
+            
+        case .changed:
+            interactor.shouldFinish = progress > mapBasedSearchVC.sceneTransitionProgressThreshold
+            interactor.update(progress)
+            
+        case .ended:
+            interactor.hasStarted = false
+            interactor.shouldFinish ? interactor.finish() : interactor.cancel()
+            
+        case .cancelled:
+            interactor.hasStarted = false
+            interactor.cancel()
+            
+        default:
+            break
+        }
+    }
+    
 }
