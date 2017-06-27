626d5619e731693f903cc240d9b99e25458366df
Trevor Beasty <trevor.beasty@gmail.com>
2017-06-27 10:09:26 -0400

    refactor transition classes; implement transitions for 'map to vendor options'

---
+++ Uncharted/Uncharted/TJBMapToVendorOptionsTransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  TJBMapToVendorOptionsTransitionManager.swift
+//  Uncharted
+//
+//  Created by Trevor Beasty on 6/27/17.
+//  Copyright © 2017 BeastlyCreations. All rights reserved.
+//
+
+import UIKit
+
+class TJBMapToVendorOptionsTransitionManager: NSObject {
+
+}
---
+++ Uncharted/Uncharted/TJBVendorOptionsPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  TJBVendorOptionsPresentationController.swift
+//  Uncharted
+//
+//  Created by Trevor Beasty on 6/26/17.
+//  Copyright © 2017 BeastlyCreations. All rights reserved.
+//
+
+import UIKit
+
+class TJBVendorOptionsPresentationController: UIPresentationController {
+
+}
